4fa2da7bf95abcb3ac07f7e754c8a034d8c7a2d7
SamMalcolm <sam_malcolm@live.com.au>
2020-10-29 13:33:35 +1100

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Samuel Malcolm on 29/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
